4bdaca30bf539f31db6063a3f2d4523c78a720a5
PD1ce <padeisinger@gmail.com>
2015-02-04 16:21:22 -0600

    Mostly working, 2 bugs: Holding on the left causes collision.  Also, need to remove objects in the right order

--- TestingMoves/HomeViewController.swift
+++ TestingMoves/HomeViewController.swift
@@ -7,41 +7,88 @@
 //
 
 import UIKit
+import Foundation
 
 class HomeViewController: UIViewController, UIGestureRecognizerDelegate {
 
     
-    @IBOutlet weak var squareView: SquareHeroView!
-    var oldPos: CGPoint!
-    var fallingObjects: [UIView]!
+    @IBOutlet weak var squareLabel: UILabel!
+    @IBOutlet weak var startGameButton: UIButton!
+    var squareView: SquareHeroView!
+    
+    var highScore: UInt8!
+    
+    var updateTimer: NSTimer!
+    var createSquareTimer: NSTimer!
+    
+    var tapRecognizer: UITapGestureRecognizer!
+    var screenPressRecognizer: UILongPressGestureRecognizer!
+    
+    var fallingObjects: [SquareHeroView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        squareView.backgroundColor = UIColor(red: 0, green: 0, blue: 1, alpha: 1)
         
-        /** Setup of Gesture Recognizers **/
-        let tapRecognizer = UITapGestureRecognizer(target: self, action: Selector("handleTap:"))
+        highScore = 0
+        tapRecognizer = UITapGestureRecognizer(target: self, action: Selector("handleTap:"))
         tapRecognizer.delegate = self
         self.view.addGestureRecognizer(tapRecognizer)
-        let screenPressRecognizer = UILongPressGestureRecognizer(target: self, action: Selector("holdSquare:"))
+        screenPressRecognizer = UILongPressGestureRecognizer(target: self, action: Selector("holdSquare:"))
         screenPressRecognizer.delegate = self
         screenPressRecognizer.minimumPressDuration = 0.05
         self.view.addGestureRecognizer(screenPressRecognizer)
-        fallingObjects.append(squareView)
-       
+        
+        tapRecognizer.enabled = false
+        screenPressRecognizer.enabled = false
+        
+        squareView = SquareHeroView(frame: CGRect(x: 162.5, y: 617, width: 50, height: 50))
+        squareView.backgroundColor = UIColor(red: 0, green: 0, blue: 1, alpha: 1)
+        
+        // Actually displays now
+        self.view.addSubview(squareView)
+        squareView.hidden = false
     }
     
     func updatePositions() {
         for object in fallingObjects {
-            if object.isKindOfClass(SquareHeroView) {
-                
+            object.updateFallingPosition()
+            
+            //Place Holder, makes sense but yuck
+            //Doesnt work with variable speed
+            if object.topY >= 667 {
+                highScore!++
+                object.removeFromSuperview()
+                fallingObjects.removeAtIndex(0)
             }
         }
+        squareView.updatePosition()
+        //println("leftX: \(squareView.leftX), rightX:\(squareView.rightX), topY: \(squareView.topY), bottomY: \(squareView.bottomY)")
     }
     
     func handleCollisions() {
         for object in fallingObjects {
+            if object.detectCollision(squareView) {
+                println("Collision detected")
+                // Exit Game
+                
+                //necessary?
+                for object in fallingObjects {
+                    object.removeFromSuperview()
+                }
             
+                updateTimer.invalidate()
+                createSquareTimer.invalidate()
+                
+                fallingObjects.removeAll(keepCapacity: false)
+                tapRecognizer.enabled = false
+                screenPressRecognizer.enabled = false
+                
+                squareLabel.text = "You lose! Score: \(highScore)"
+                highScore = 0
+                squareLabel.hidden = false
+                startGameButton.titleLabel?.text = "Try Again?"
+                startGameButton.hidden = false
+            }
         }
     }
     
@@ -64,6 +111,56 @@ class HomeViewController: UIViewController, UIGestureRecognizerDelegate {
         println("User Tapped, Fire!")
     }
 
+    @IBAction func startGameTapped(sender: AnyObject) {
+        // Move all this to start!
+        
+        fallingObjects = [SquareHeroView]()
+        let newFallingObject = SquareHeroView()
+        newFallingObject.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        fallingObjects.append(newFallingObject)
+        
+        /** Enabling of Gesture Recognizers **/
+        tapRecognizer.enabled = true
+        screenPressRecognizer.enabled = true
+        
+        //Remove button and such
+        //squareLabel.removeFromSuperview()
+        //startGameButton.removeFromSuperview()
+        
+        squareLabel.hidden = true
+        startGameButton.hidden = true
+        
+        updateTimer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: "fireUpdater:", userInfo: nil, repeats: true)
+         createSquareTimer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "fireSquareCreator:", userInfo: nil, repeats: true)
+        
+        //println("Timer should be initialized")
+        //println("\(timer)")
+        
+    }
+    
+    func fireUpdater(timer: NSTimer) {
+        updatePositions()
+        handleCollisions()
+        //println("Updater Fired")
+    }
+    func fireSquareCreator(timer: NSTimer) {
+        let xVal = CGFloat(arc4random_uniform(326))
+        let yVal = CGFloat(-50)
+        let randWidth = CGFloat(arc4random_uniform(50) + 50)
+        let randHeight = CGFloat(arc4random_uniform(50) + 50)
+        let red = CGFloat(arc4random_uniform(2))
+        let green = CGFloat(arc4random_uniform(2))
+        //let fallingSpeed = UInt8(arc4random_uniform(10) + 6)
+        let fallingSpeed = UInt8(10 + highScore)
+        var enemySquare = SquareHeroView(frame: CGRect(x: xVal, y: yVal, width: randWidth, height: randHeight))
+        enemySquare.backgroundColor = UIColor(red: red, green: green, blue: 0, alpha: 1)
+        enemySquare.fallingSpeed = fallingSpeed
+        self.view.addSubview(enemySquare)
+        fallingObjects.append(enemySquare)
+        //println("Square Creator Fired")
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- TestingMoves/SquareHeroView.swift
+++ TestingMoves/SquareHeroView.swift
@@ -14,11 +14,37 @@ class SquareHeroView : UIView {
     var rightX: CGFloat!
     var topY: CGFloat!
     var bottomY: CGFloat!
+    var fallingSpeed: UInt8!
     
+    //Can't be statically put as 25
+    // BUG
     func updatePosition() {
         leftX =     self.center.x - 25
         rightX =    self.center.x + 25
         topY =      self.center.y - 25
         bottomY =   self.center.y + 25
+    }
+    
+    func updateFallingPosition() {
+        if fallingSpeed != nil {
+            self.center.y += CGFloat(fallingSpeed!)
+        }
+        leftX =     self.center.x - 25
+        rightX =    self.center.x + 25
+        topY =      self.center.y - 25
+        bottomY =   self.center.y + 25
+    }
+    
+    //** BUG!  When all the way left it can collide! **//
+    func detectCollision(square: SquareHeroView) -> Bool {
+        if (self.leftX <= square.rightX) && (self.rightX >= square.rightX) && (self.bottomY >= square.topY) {
+            return true
+        }
+        if (self.rightX >= square.leftX) && (self.leftX <= square.leftX) && (self.bottomY >= square.topY) {
+            return true
+        }
+        //Might be all cases?
+        
+        return false
     }
 }
